2d4820e586a61aaa614ae1be3fa57994e03c4d51
Thomas Eley <thomas-eley@hotmail.co.uk>
2020-10-10 12:22:05 +0400

    Dijkstra Final Commit

--- dijkstra.py
+++ dijkstra.py
@@ -1,93 +1,77 @@
-#Sys is a system module which can be used to determine the largest number usable by the system
+#Imports system module to access the largest usuable number (Substitute for Infinity)
 from sys import maxsize
-#Creates a class to be used for the graph
 class graph:
-#Class Initialisation
+#Initialization
     def __init__(self, verts, matrix, title):
-#Sets class variables for the vertices of the graph, the graph adjacency matrix, and the title of the graph
+#Takes a list of the vertices
         self.__verts = verts
+#Takes a 2D array containing the information of the adjacency matrix
         self.__matrix = matrix
         self.__title = title
 
-#Method used to call dijkstra's algorithm and properly display the results (Takes the initial vertex as an argument)
-    def shortestPath(self, vert):
-#Uses the .index() function to find the index of the vertex
-        vertIndex = self.__verts.index(vert)
-#Creats a list to contain all of the weights for the vertices (Initialised to the highest possible value)
-        self.__dist = [maxsize for item in self.__verts]
-#Sets the weight from the intitial vertex to 0 (Because there is no distance to get to itself)
-        self.__dist[vertIndex] = 0
-#Creates a list to contain all of the previous vertex (The previous vertex in the shortest path)
-        self.__prev = ["-" for item in self.__verts]
-#Sets the previous vertex for the initial vertex to SI (Starting Itemm)
-        self.__prev[vertIndex] = "SI"
-#Creats a list to contain boolean values which can tell the user if a value has been checked yet
-        self.__finished = [False for item in self.__verts]
-#Calls other methods within the class to carry out the main portion of dijkstra's algorithm
-        self.recurseThrough(vert)
-        self.displayShortest(vert)
+#Method used to find the shortest route to all vertices in a graph (From startV)
+    def shortestPath(self, startV):
+#Sets a list to contain all of the shortest distances
+#At the beginning all values set to infinity (Maxsize)
+        self.__dists = [maxsize for vert in self.__verts]
+#The distance value of the vertex being searched is set to 0 (This is the only vertex that doesn't need to be checked)
+        self.__dists[self.__verts.index(startV)] = 0
+#Sets a list to contain all of the previous vertices for the shortest path
+#At the beginning all values set to null (-)
+        self.__prev = ["-" for vert in self.__verts]
+#The previous value of the vertex being searched is set to SI (Search Item)
+        self.__prev[self.__verts.index(startV)] = "SI"
+#Sets a list to contain boolean values indicating if a vertex has been checked
+#At the beginning all values are set to False
+        self.__finished = [False for vert in self.__verts]
+#Loops through each vertex
+        for vert in self.__verts:
+#Calls the nextVert method to determine which vertex to check next
+            next = self.nextVert()
+#Takes the index of the next vertex to check
+            nIndex = self.__verts.index(next)
+#Sets te next vertex to finished
+            self.__finished[nIndex] = True
+#Loops through each sub-vertex
+            for uert in self.__verts:
+#Takes the index value of the new vertex
+                uIndex = self.__verts.index(uert)
+                self.dijkstra(nIndex, uIndex, next)
+#The method used to correctly display the shortest routes is called
+        self.displayShortest(startV)
 
-#Method that uses recursion to move through the graph and carry out Dijkstra's algorithm
-    def recurseThrough(self, startVert):
-#Calls the method that carries out Dijkstra's algorithm on all of the directly adjacent vertices
-        self.doLayer(startVert)
-#Sets the vertex to True in finished to indicat that the vertex has already had the algorithm applied to it
-        self.__finished[self.__verts.index(startVert)] = True
-#Calls the method that determines the order of adjacent vertices that need to have the algorithm applied to them
-        adjacent = self.order(self.__matrix[self.__verts.index(startVert)])
-#Uses recursion to carry out the algorithm on the adjacent items
-        for item in adjacent:
-            if not self.__finished[self.__verts.index(item)]:
-                self.recurseThrough(item)
+#Method to carry out the core purpose of dijkstra's algorithm
+    def dijkstra(self, curr, new, vert):
+#Using the 2d array self.__matrix, first the weight from the matrix is taken for the current values
+#If the weight exists, and the item has not been checked yet the main portion of dijkstras algorithm is called
+        if self.__matrix[curr][new] != "0" and self.__finished[new] != True:
+#Checks if the weight to the previous item added to the new weight is smaller than the current weight
+            if self.__dists[curr] + int(self.__matrix[curr][new]) < self.__dists[new]:
+#If so, the current weight is replaced with the weight to the previous weight added to the new weight
+                self.__dists[new] = self.__dists[curr] + int(self.__matrix[curr][new])
+#and the current previous item is replaced with the current item
+                self.__prev[new] = vert
 
-#Method used to carry out the algorithm on all of the adjacent items to the passed "vert"
-    def doLayer(self, vert):
-#Uses the .index() function to find the index of the vertex
-        vertIndex = self.__verts.index(vert)
-#Calls the method that determines the order in which the adjacent vertices need to have the algorithm applied to
-        order = self.order(self.__matrix[vertIndex])
-#Carries out the algorithm on the adjacent vertices in order
-        for item in order:
-            self.dijkstra(item, vert)
-
-#Method used to determine the order in which the vertices in an array need to be traversed
-    def order(self, array):
-#Creates a local array (Otherwise the class variable is altered) from the line taken from the matrix
-        localarray = [item for item in array]
-#Sorts the matrix line
-        newArray = sorted(localarray)
-#Creates an empty list to have the index values of items appended to
-        indexes = []
-#For loop to append the index values
-        for i in range(len(newArray)):
-#Only appends the index if the value is more than zero (indicating a link between the vertices in the graph)
-            if newArray[i] != "0":
-                index = localarray.index(newArray[i])
-                indexes.append(index)
-                localarray[index] = "-"
-#Creats an empty list to have the letter values of the indices appended to
-        letterOrder = []
-#Appends the letter value of each of the indices to the list
-        for index in indexes:
-            letterOrder.append(self.__verts[index])
-        return letterOrder
-
-#Method used to carry out the main portion of dijkstra's algorithm
-    def dijkstra(self, curr, comp):
-#Sets the distance between the two passed vertices using the 2d matrix array matrix[current vertex][comparison vertex]
-        dist = int(self.__matrix[self.__verts.index(comp)][self.__verts.index(curr)])
-#If the distance is zero, nothing happens
-        if dist != 0:
-#Otherwise, if the distance to the comparison item + the distance between the items being compared is less than the current smallest distance
-            if dist + self.__dist[self.__verts.index(comp)] < self.__dist[self.__verts.index(curr)]:
-#The current smallest distance is updated
-                self.__dist[self.__verts.index(curr)] = dist + self.__dist[self.__verts.index(comp)]
-#and the current previous vertex is updated to the comparison item
-                self.__prev[self.__verts.index(curr)] = comp
+#Method used to determine which vertex to check next
+    def nextVert(self):
+#Sets the base distance to infinity (maxsize)
+        dist = maxsize
+#Loops through each vertex
+        for vert in self.__verts:
+#Sets an index item to the vertex index
+            index = self.__verts.index(vert)
+#If the current dist is smaller than the base dist and the vertex hasn't been checked yet
+            if self.__dists[index] < dist and self.__finished[index] == False:
+#The base distance is replaced with the current distance
+                dist = self.__dists[index]
+#Sets the index of the next item to the index of the new distance
+                nextIndex = index
+#Returns the vertex value for nextIndex
+        return self.__verts[nextIndex]
 
 #Method used to display the results from Dijkstra's algorithm
     def displayShortest(self, startV):
-        dists = self.__dist
+        dists = self.__dists
         prevs = self.__prev
 #Line of Equals signs to help with testing (Distinguish between algorithm calls)
         print("============================================================")
@@ -103,9 +87,9 @@ class graph:
 #Otherwise
             else:
 #The method used to determine the route of the shortest path is called
-                thePath = self.findPath(self.__prev,item)
+                thePath = self.findPath(prevs,item)
 #The shortest distance is set to the vertex item within the dists list
-                theDistance = self.__dist[self.__verts.index(item)]
+                theDistance = dists[self.__verts.index(item)]
 #Prints all of the data
                 print("From", startV, "to", item)
                 print("Shortest Path :", " to ".join(list(thePath)))
@@ -135,7 +119,7 @@ myG = graph(myVerts, myMatrix, "Graph 1 (4 Vertices)")
 myG.shortestPath("A")
 
 myVerts2 = ["A", "B", "C", "D", "E"]
-myMatrix2 = [["0","1","3","0","11"], ["1","0","0","0","2"], ["3","0","0","0","7"], ["0","0","0","0","1"], ["11","2","7","1 ","0"]]
+myMatrix2 = [["0","1","3","0","11"], ["1","0","0","0","2"], ["3","0","0","0","7"], ["0","0","0","0","1"], ["11","2","7","1","0"]]
 myG2 = graph(myVerts2, myMatrix2, "Graph 2 (5 Vertices)")
 myG2.shortestPath("A")
 
